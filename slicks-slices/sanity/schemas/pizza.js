2fed9f4842c379e806320ffe89898a4456659252
Zak Laughton <me@zaklaughton.dev>
2020-10-06 03:11:00 -0500

    completed CMS set up module

--- slicks-slices/sanity/schemas/pizza.js
+++ slicks-slices/sanity/schemas/pizza.js
@@ -1,4 +1,5 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 
 export default {
   // Computer Name
@@ -34,6 +35,7 @@ export default {
       type: 'number',
       description: 'Price of the pizza in cents',
       validation: (Rule) => Rule.min(1000),
+      inputComponent: PriceInput,
       // TODO: Add custom input component
     },
     {
