557747b7b0f0330eba2c72c211bd55ba2d9676dc
Zak Laughton <me@zaklaughton.dev>
2020-10-07 08:37:26 -0500

    add pizza list with images in grid

--- slicks-slices/gatsby/gatsby-config.js
+++ slicks-slices/gatsby/gatsby-config.js
@@ -1,7 +1,24 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
   siteMetadata: {
     title: `Slick's Slices`,
     siteUrl: `https://gatsby.pizza`,
     description: `The best pizza place in Hamilton!`,
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'wm2kcgnb',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
