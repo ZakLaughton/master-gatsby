f9119f1026878319ab571e24de39830e170ae7db
Zak Laughton <zlaughton@gmail.com>
2020-11-10 08:01:55 -0600

    add slice master and pizza data to home page

--- slicks-slices/gatsby/src/pages/index.js
+++ slicks-slices/gatsby/src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ItemGrid from '../components/ItemGrid';
 import Layout from '../components/Layout';
 import LoadingGrid from '../components/LoadingGrid';
 import Nav from '../components/Nav';
@@ -8,10 +9,15 @@ import useLatestData from '../utils/useLatestData';
 function CurrentlySlicing({ slicemasters }) {
   return (
     <div>
+      <h2 className="center">
+        <span className="mark tilt">Slicemasters On</span>
+      </h2>
+      <p>Standing by, ready to slice you up!</p>
       {!slicemasters && <LoadingGrid count={4} />}
       {slicemasters && !slicemasters?.length && (
         <p>No one is working right now!</p>
       )}
+      {slicemasters?.length && <ItemGrid items={slicemasters} />}
     </div>
   );
 }
@@ -19,8 +25,13 @@ function CurrentlySlicing({ slicemasters }) {
 function HotSlices({ hotSlices }) {
   return (
     <div>
+      <h2 className="center">
+        <span className="mark tilt">Hot Slices</span>
+      </h2>
+      <p>Come on by, buy the slice!</p>
       {!hotSlices && <LoadingGrid count={4} />}
       {hotSlices && !hotSlices?.length && <p>Nothin' in the case</p>}
+      {hotSlices?.length && <ItemGrid items={hotSlices} />}
     </div>
   );
 }
